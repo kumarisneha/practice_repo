a165962921ccdfbd6ba4eb4a6c7cbd38b57fdf47
Sneha Kumari <kumarisneha102@gmail.com>
2017-03-31 14:20:58 +0530

    Print sum of same data types on a different line

---
+++ hackerrank_datatype.py
@@ -0,0 +1,16 @@
+i=10
+d=2.5
+s="HackerRank"
+# Declare second integer, double, and String variables.
+ii=int(raw_input())
+dd=float(raw_input())
+ss=raw_input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print i+ii
+# Print the sum of the double variables on a new line.
+print d+dd
+# Concatenate and print the String variables on a new line
+print s+ " " + ss
+# The 's' variable above should be printed first.
